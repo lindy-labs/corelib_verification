64de794bb1cbaecea64a8c48ec75626e4ea0f814
Jakob von Raumer <jakob@von-raumer.de>
2023-12-14 15:20:15 +0100

    progress on fixing Corelib.Serde

--- CorelibVerification/Corelib/Serde.lean
+++ CorelibVerification/Corelib/Serde.lean
@@ -168,32 +168,37 @@ aegis_prove "core::array::serialize_array_helper<core::integer::u64, core::serde
   unfold «spec_core::array::serialize_array_helper<core::integer::u64, core::serde::into_felt252_based::SerdeImpl<core::integer::u64, core::integer::u64Copy, core::integer::U64IntoFelt252, core::integer::Felt252TryIntoU64>, core::integer::u64Drop>»
   sierra_simp'
   generalize m.costs id!"core::array::serialize_array_helper<core::integer::u64,core::serde::into_felt252_based::SerdeImpl<core::integer::u64,core::integer::u64Copy,core::integer::U64IntoFelt252,core::integer::Felt252TryIntoU64>,core::integer::u64Drop>" = c
-  rintro (⟨hle,h⟩|⟨h,rfl⟩)
-  · rcases h with (⟨a,b,_,d,e,rfl,h₁,h₂,h₃⟩|⟨rfl,rfl,rfl⟩)
-    · injection h₁ with h₁; subst h₁
-      split_ifs at h₂ with hle'
-      · rcases h₂ with ⟨rfl,rfl⟩
-        have : (a.length + 2) * c ≤ gas
-        · have := Nat.add_le_add_right hle' c
-          rw [Nat.sub_add_cancel hle] at this
-          linarith
-        simp only [List.append_assoc, List.singleton_append, Sum.inl.injEq, ge_iff_le,
-          tsub_le_iff_right, exists_and_left, exists_and_right, exists_eq_left, Prod.mk.injEq,
-          and_true, exists_const, false_and, exists_false, or_false] at h₃
-        rcases h₃ with ⟨rfl,rfl⟩
-        simp only [List.length_cons, this, ge_iff_le, tsub_le_iff_right, and_true, Sum.isRight_inl,
-          ite_true]
-        rw [add_mul, one_mul, add_mul, Nat.succ_mul, one_mul, Nat.sub_sub]
-        simp only [List.map_cons, and_true]
-        ac_rfl
-      · have : ¬ (a.length + 2) * c ≤ gas
-        · rw [not_le] at hle' ⊢
-          have := Nat.add_lt_add_right hle' c
-          rw [Nat.sub_add_cancel hle] at this
-          linarith
-        simp only [List.length_cons, this, ge_iff_le, ite_false]
-        aesop
-    · simp [hle]
+  rintro (⟨_,_,hle,h,h'⟩|⟨h,rfl⟩)
+  · rw [← List.length_pos] at h
+    rcases h with (⟨h,rfl,rfl⟩|⟨rfl,rfl,rfl⟩)
+    · rcases h' with (⟨a,b,_,h₁,h₂,h₃⟩|h')
+      · cases h₁
+        split_ifs at h₂ with hle'
+        · rcases h₂ with ⟨rfl,rfl⟩
+          have : (data.length + 1) * c ≤ gas
+          · have := Nat.add_le_add_right hle' c
+            simp only [Nat.sub_add_cancel hle, List.length_tail, ge_iff_le, Nat.sub_add_cancel h] at this
+            linarith
+          simp only [List.map_tail, List.append_assoc, List.singleton_append, Sum.inl.injEq,
+            ge_iff_le, List.length_tail, tsub_le_iff_right, exists_and_left, exists_and_right,
+            exists_eq_left, Prod.mk.injEq, and_true, exists_const, false_and, or_false] at h₃
+          rcases h₃ with ⟨rfl,rfl⟩
+          simp only [this, ge_iff_le, tsub_le_iff_right, Sum.inl.injEq, Prod.mk.injEq,
+            List.append_cancel_left_eq, and_true, Sum.isRight_inl, ite_true, Nat.sub_add_cancel h]
+          constructor
+          · rw [add_mul, one_mul, add_comm, Nat.sub_sub]
+          · cases data
+            · simp at h
+            · simp
+        · have : ¬ (data.length + 1) * c ≤ gas
+          · rw [not_le] at hle' ⊢
+            have := Nat.add_lt_add_right hle' c
+            simp only [Nat.sub_add_cancel hle, List.length_tail, ge_iff_le, Nat.sub_add_cancel h] at this
+            linarith
+          simp only [List.length_cons, this, ge_iff_le, ite_false]
+          aesop
+      · aesop
+    · aesop
   · have : ¬ (data.length + 1) * c ≤ gas
     · rw [not_le, Nat.add_mul, one_mul]; apply Nat.lt_add_left h
     simp [this]
@@ -210,31 +215,37 @@ aegis_prove "core::array::serialize_array_helper::<core::felt252, core::Felt252S
   unfold «spec_core::array::serialize_array_helper::<core::felt252, core::Felt252Serde, core::felt252Drop>»
   sierra_simp'
   generalize m.costs id!"core::array::serialize_array_helper::<core::felt252, core::Felt252Serde, core::felt252Drop>" = c
-  rintro (⟨hle,h⟩|⟨h,rfl⟩)
-  · rcases h with (⟨a,b,_,d,e,rfl,h₁,h₂,h₃⟩|⟨rfl,rfl,rfl⟩)
-    · injection h₁ with h₁; subst h₁
-      split_ifs at h₂ with hle'
-      · rcases h₂ with ⟨rfl,rfl⟩
-        have : (a.length + 2) * c ≤ gas
-        · have := Nat.add_le_add_right hle' c
-          rw [Nat.sub_add_cancel hle] at this
-          linarith
-        simp only [List.append_assoc, List.singleton_append, Sum.inl.injEq, ge_iff_le,
-          tsub_le_iff_right, exists_and_left, exists_and_right, exists_eq_left, Prod.mk.injEq,
-          and_true, exists_const, false_and, exists_false, or_false] at h₃
-        rcases h₃ with ⟨rfl,rfl⟩
-        simp only [List.length_cons, this, ge_iff_le, tsub_le_iff_right, and_true, Sum.isRight_inl,
-          ite_true]
-        rw [add_mul, one_mul, add_mul, Nat.succ_mul, one_mul, Nat.sub_sub]
-        ac_rfl
-      · have : ¬ (a.length + 2) * c ≤ gas
-        · rw [not_le] at hle' ⊢
-          have := Nat.add_lt_add_right hle' c
-          rw [Nat.sub_add_cancel hle] at this
-          linarith
-        simp only [List.length_cons, this, ge_iff_le, ite_false]
-        aesop
-    · simp [hle]
+  rintro (⟨_,_,hle,h,h'⟩|⟨h,rfl⟩)
+  · rw [← List.length_pos] at h
+    rcases h with (⟨h,rfl,rfl⟩|⟨rfl,rfl,rfl⟩)
+    · rcases h' with (⟨a,b,_,h₁,h₂,h₃⟩|h')
+      · cases h₁
+        split_ifs at h₂ with hle'
+        · rcases h₂ with ⟨rfl,rfl⟩
+          have : (data.length + 1) * c ≤ gas
+          · have := Nat.add_le_add_right hle' c
+            simp only [Nat.sub_add_cancel hle, List.length_tail, ge_iff_le, Nat.sub_add_cancel h] at this
+            linarith
+          simp only [List.map_tail, List.append_assoc, List.singleton_append, Sum.inl.injEq,
+            ge_iff_le, List.length_tail, tsub_le_iff_right, exists_and_left, exists_and_right,
+            exists_eq_left, Prod.mk.injEq, and_true, exists_const, false_and, or_false] at h₃
+          rcases h₃ with ⟨rfl,rfl⟩
+          simp only [this, ge_iff_le, tsub_le_iff_right, Sum.inl.injEq, Prod.mk.injEq,
+            List.append_cancel_left_eq, and_true, Sum.isRight_inl, ite_true, Nat.sub_add_cancel h]
+          constructor
+          · rw [add_mul, one_mul, add_comm, Nat.sub_sub]
+          · cases data
+            · simp at h
+            · simp
+        · have : ¬ (data.length + 1) * c ≤ gas
+          · rw [not_le] at hle' ⊢
+            have := Nat.add_lt_add_right hle' c
+            simp only [Nat.sub_add_cancel hle, List.length_tail, ge_iff_le, Nat.sub_add_cancel h] at this
+            linarith
+          simp only [List.length_cons, this, ge_iff_le, ite_false]
+          aesop
+      · aesop
+    · aesop
   · have : ¬ (data.length + 1) * c ≤ gas
     · rw [not_le, Nat.add_mul, one_mul]; apply Nat.lt_add_left h
     simp [this]
@@ -247,7 +258,7 @@ erase_aesop_rules [cases Sum]
 
 aegis_spec "core::array::ArraySerde::<core::felt252, core::Felt252Serde, core::felt252Drop>::serialize" :=
   fun m _ gas data str _ gas' ρ =>
-  let c := m.costs id!"core::serialize_array_helper<core::felt252, core::serde::Felt252Serde, core::felt252Drop>"
+  let c := m.costs id!"core::array::serialize_array_helper<core::felt252,core::Felt252Serde,core::felt252Drop>"
   if (data.length + 1) * c ≤ gas then
     gas' = gas - (data.length + 1) * c ∧ ρ = .inl (str ++ [((data.length : Sierra.UInt32).cast : F)] ++ data, ())
   else ρ.isRight
@@ -270,7 +281,7 @@ aegis_spec "core::array::deserialize_array_helper::<core::integer::u128, core::s
 aegis_prove "core::array::deserialize_array_helper::<core::integer::u128, core::serde::into_felt252_based::SerdeImpl::<core::integer::u128, core::integer::u128Copy, core::integer::U128IntoFelt252, core::integer::Felt252TryIntoU128>, core::integer::u128Drop>" :=
   fun m _ gas s curr r _ gas' ρ => by
   unfold «spec_core::array::deserialize_array_helper::<core::integer::u128, core::serde::into_felt252_based::SerdeImpl::<core::integer::u128, core::integer::u128Copy, core::integer::U128IntoFelt252, core::integer::Felt252TryIntoU128>, core::integer::u128Drop>»
-  generalize Metadata.costs m id!"core::serde::deserialize_array_helper<core::integer::u128,core::serde::U128Serde,core::integer::u128Drop>" = c
+  generalize Metadata.costs m id!"core::array::deserialize_array_helper::<core::integer::u128, core::serde::into_felt252_based::SerdeImpl::<core::integer::u128, core::integer::u128Copy, core::integer::U128IntoFelt252, core::integer::Felt252TryIntoU128>, core::integer::u128Drop>" = c
   rintro ⟨hd,_,_,_,_,_,_,_,(⟨hle,h⟩|⟨h,rfl⟩)⟩
   -- Case: Enough gas for one run
   · rcases h with (⟨rfl,rfl,rfl⟩|⟨hne,h⟩)
@@ -315,7 +326,7 @@ aegis_prove "core::array::deserialize_array_helper::<core::integer::u128, core::
               aesop
             · conv_rhs => rw [← Nat.succ_pred_eq_of_pos hr']
             · conv_rhs => rw [← Nat.succ_pred_eq_of_pos hr']
-          · rcases hrec with ⟨rfl,rfl⟩; clear hrec
+          · rcases hrec with ⟨rfl,rfl⟩
             rw [ite_prop_iff_or]; left
             rw [not_and_or] at hrs; rcases hrs with (hrs|hrs)
             · constructor
